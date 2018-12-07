6aa9861a07e2262f09e3564ed72c8e19a7e9b391
justin-nguyen03 <38301747+justin-nguyen03@users.noreply.github.com>
2018-12-07 14:48:45 -0800

    Add files via upload
    Change checkRoom to use Calendar comparison

--- Database.java
+++ Database.java
@@ -1,111 +1,132 @@
-package hotelReservationSystem;
+package management;
 
 import java.awt.List;
+import java.io.FilterInputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.concurrent.TimeUnit;
 
+import javax.xml.transform.Templates;
+
 public class Database {
-
-	private ArrayList<Account> accounts;
-	private ArrayList<Room> rooms;
-
+	
+	private ArrayList<Account>accounts;
+	private ArrayList<Room>rooms;
+	
 	public Database() {
 		accounts = new ArrayList<Account>();
 		rooms = new ArrayList<Room>();
 	}
-
-	public String getAccounts() {
-		if (accounts.size() != 0) {
-			for (Account a : accounts) {
-				return a.getUsername();
-			}
+	
+	public String getAccounts(){
+		if(accounts.size() != 0){
+		for(Account a: accounts) {
+			return a.getUsername();
+		 }
 		}
-		return "no accounts";
-
+			return "no accounts";
+		
 	}
-
+	
 	public void addAccounts(Account a) {
 		accounts.add(a);
 	}
-
+	
 	public boolean checkUsername(String username) {
-		for (int i = 0; i < accounts.size(); i++) {
-			if (accounts.get(i).getUsername().equals(username)) {
+		for(int i = 0; i < accounts.size() ; i++)
+		{
+			if(accounts.get(i).getUsername().equals(username))
+			{
 				return true;
 			}
 		}
 		return false;
 	}
-
+	
 	public boolean checkLogin(String u, String p) {
-		for (int i = 0; i < accounts.size(); i++) {
-			if (accounts.get(i).getUsername().equals(u)) {
-				if (accounts.get(i).getPassword().equals(p)) {
+		for(int i = 0; i < accounts.size() ; i++) {
+			if(accounts.get(i).getUsername().equals(u))
+			{
+				if(accounts.get(i).getPassword().equals(p))
+				{
 					return true;
-				} else {
+				}
+				else {
 					return false;
 				}
 			}
 		}
 		return false;
 	}
-
+	
 	public ArrayList<Room> getRooms() {
 		return rooms;
 	}
-
-	public boolean compareDay(String inputStartDate, String currentStartDate) throws ParseException {
-		Date sDate = new SimpleDateFormat("MM/dd/yyyy").parse(inputStartDate);
-		Date eDate = new SimpleDateFormat("MM/dd/yyyy").parse(currentStartDate);
-		if (eDate.getMonth() <= sDate.getMonth()) {
-			if (eDate.getDay() <= sDate.getDay()) {
-				if (eDate.getYear() <= sDate.getYear()) {
-					System.out.println("Passed");
-					return true;
-				}
-			}
+	
+	public int compareDay(String inputDate, String currentDate) throws ParseException{
+		Date sDate = new SimpleDateFormat("MMddyyyy").parse(inputDate );
+		Date eDate = new SimpleDateFormat("MMddyyyy").parse(currentDate);
+		Calendar calS = new GregorianCalendar(sDate.getYear(), sDate.getMonth(), sDate.getDay());
+		Calendar calE = new GregorianCalendar(eDate.getYear(), eDate.getMonth(), eDate.getDay());
+		boolean compare = calS.equals(calE);
+		if(compare == true) {
+			return 1;
 		}
-		System.out.println("Failed");
-		return false;
+		return -1;
 	}
-
-	public long getLength(String start, String end) throws ParseException {
-		Date sDate = new SimpleDateFormat("MM/dd/yyyy").parse(start);
-		Date eDate = new SimpleDateFormat("MM/dd/yyyy").parse(end);
-		long difference = eDate.getTime() - sDate.getTime();
-		return TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
+	
+	public int compareBeforeDay(String inputStartDate, String currentEndDate) throws ParseException{
+		Date sDate = new SimpleDateFormat("MMddyyyy").parse(inputStartDate );
+		Date eDate = new SimpleDateFormat("MMddyyyy").parse(currentEndDate);
+		Calendar calS = new GregorianCalendar(sDate.getYear(), sDate.getMonth(), sDate.getDay());
+		Calendar calE = new GregorianCalendar(eDate.getYear(), eDate.getMonth(), eDate.getDay());
+		boolean compare = calS.before(calE);
+		if(compare == true) {
+			return 1;
+		}
+		return -1;
 	}
-
+	
+	public int compareAfterDay(String inputStartDate, String currentStartDate) throws ParseException{
+		Date sDate = new SimpleDateFormat("MMddyyyy").parse(inputStartDate );
+		Date eDate = new SimpleDateFormat("MMddyyyy").parse(currentStartDate);
+		Calendar calS = new GregorianCalendar(sDate.getYear(), sDate.getMonth(), sDate.getDay());
+		Calendar calE = new GregorianCalendar(eDate.getYear(), eDate.getMonth(), eDate.getDay());
+		boolean compare = calS.after(calE);
+		if(compare == true)
+		{
+			return 1;
+		}
+		return -1;
+	}
+	
 	public void addRooms(Room r) {
 		rooms.add(r);
 	}
-
+	
 	public java.util.List<Integer> checkRoom(String startDate, String endDate) throws ParseException {
-
-		java.util.List<Integer> openRooms = new ArrayList<Integer>(
-				Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
-		// ArrayList<Integer> openRooms = new ArrayList<>();
-		// int[] open = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-		System.out.println("this is before remove  " + openRooms);
-		for (int i = 0; i < rooms.size(); i++) {
-			if (compareDay(rooms.get(i).getStartDate(), startDate) == false) {
-				/*
-				 * for(int roomNumber : open) { openRooms.add(roomNumber); }
-				 */
-				return openRooms;
-			} else if (compareDay(rooms.get(i).getStartDate(), startDate) == true) {
-				if (getLength(rooms.get(i).getStartDate(), startDate) < getLength(startDate, endDate)) {
-					int currentRoom = rooms.get(i).getRoomNumber();
-					openRooms.remove(currentRoom);
-				}
+		java.util.List<Integer> openRooms = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
+		
+		for(int i = 0; i < rooms.size(); i++)
+		{
+			String currentStartDate = rooms.get(i).getStartDate();
+			String currentEndDate = rooms.get(i).getEndDate();
+			int roomNumber = rooms.get(i).getRoomNumber();
+			
+			if(compareDay(startDate, currentStartDate)== 1 || compareDay(endDate, currentStartDate)==1 || compareDay(endDate, currentEndDate)==1) {
+				openRooms.remove(roomNumber);
 			}
-
+			else if((compareAfterDay(startDate, currentStartDate) ==1 && compareBeforeDay(startDate, currentEndDate) ==1) || 
+					(compareAfterDay(endDate, currentStartDate) ==1 && compareBeforeDay(endDate, currentEndDate) ==1)||
+					(compareAfterDay(endDate, currentEndDate)==1 && compareBeforeDay(startDate, currentEndDate)==1)) {
+				openRooms.remove(roomNumber); 	
+			}
 		}
-		System.out.println("This is after " + openRooms);
 		return openRooms;
-	}
+	}			
 }
